7dfea4871b096d82298cf1c3b008a92f37c6bc25
Michael Jones <michael@autorefactorer.com>
2018-05-05 15:32:21 -0500

    Converted to python3

--- brscraper.py
+++ brscraper.py
@@ -1,6 +1,6 @@
 
 from bs4 import BeautifulSoup
-import urllib2
+import urllib.request
 
 class BRScraper:
     def __init__(self, server_url="http://www.baseball-reference.com/"):
@@ -25,14 +25,14 @@ class BRScraper:
 
         if isinstance(table_ids, str): table_ids = [table_ids]
 
-        soup = BeautifulSoup(urllib2.urlopen(self.server_url + resource),"html.parser")
+        soup = BeautifulSoup(urllib.request.urlopen(self.server_url + resource),"html.parser")
         tables = soup.find_all(is_parseable_table)
         data = {}
 
         # Read through each table, read headers as dictionary keys
         for table in tables:
             if table_ids != None and table["id"] not in table_ids: continue
-            if verbose: print "Processing table " + table["id"]
+            if verbose: print ("Processing table " + table["id"])
             data[table["id"]] = []
             headers = table.find("thead").find_all("th")
             header_names = []
@@ -48,9 +48,9 @@ class BRScraper:
                         header_name = base_header_name + "_" + str(i)
                     if verbose:
                         if base_header_name == "":
-                            print "Empty header relabeled as %s" % header_name
+                            print ("Empty header relabeled as %s" % header_name)
                         else:
-                            print "Header %s relabeled as %s" % (base_header_name, header_name)
+                            print ("Header %s relabeled as %s" % (base_header_name, header_name))
                 else:
                     header_name = base_header_name
                 header_names.append(header_name)
